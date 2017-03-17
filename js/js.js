fa5130ded75cfa7c9d485158012f5dedd9b5986b
Matthew Kustes <mattkustes@gmail.com>
2017-03-16 21:00:46 -0400

    changed hero images, added insta feed, added hamburger, changed layout

--- js/js.js
+++ js/js.js
@@ -13,3 +13,36 @@ function init() {
     });
 }
 window.onload = init();
+
+// Hamburger menu
+
+
+if ($(window).width() < 600) {
+        $( ".cross" ).hide();
+        $( ".nav" ).hide();
+        $( ".hamburger" ).click(function() {
+        $( "img.hero" )
+        $( ".nav" ).slideToggle( "slow", function() {
+        $( ".hamburger" ).hide();
+        $( ".cross" ).show();
+        });
+        });
+
+        $( ".cross" ).click(function() {
+        $( ".nav" ).slideToggle( "slow", function() {
+        $( ".cross" ).hide();
+        $( ".hamburger" ).show();
+        });
+        });   
+} else {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).hide();
+    $( ".nav" ).show();
+}
+
+
+
+// insta information
+// client id 	95ce76841080474798f24ad05df32651
+//user id 4287091103
+//http://localhost/#access_token=4287091103.95ce768.9c7d2a639dd94e31a240df10790322ce
